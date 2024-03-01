100ea730c61eeccb45e25b3d7b3834f8b4a2b928
Q <q@misell.cymru>
2024-03-01 17:42:40 +0000

    extensible address tokens

--- bdp_tokens/src/lib.rs
+++ bdp_tokens/src/lib.rs
@@ -1,20 +1,94 @@
 use std::io::{Read, Write};
-use digest::generic_array;
-use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
-use hmac::digest::OutputSizeUser;
-
-type HmacSha256 = hmac::Hmac<sha2::Sha256>;
+use byteorder::{ReadBytesExt, WriteBytesExt};
+use chacha20poly1305::{AeadCore, AeadInPlace, KeyInit};
 
 const NANOS_PER_MICRO: u32 = 1_000;
 const MICROS_PER_SEC: u128 = 1_000_000;
+
+#[repr(u64)]
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, num_enum::FromPrimitive, num_enum::IntoPrimitive)]
+pub enum ExtensionType {
+    BDPToken = 1,
+    #[num_enum(catch_all)]
+    Unknown(u64)
+}
+
+#[derive(Debug, Clone)]
+pub struct ExToken {
+    pub address_validation_data: Vec<u8>,
+    pub extensions: std::collections::BTreeMap<ExtensionType, Vec<u8>>
+}
+
+impl Default for ExToken {
+    fn default() -> Self {
+        Self {
+            address_validation_data: Vec::new(),
+            extensions: std::collections::BTreeMap::new()
+        }
+    }
+}
+
+impl ExToken {
+    pub fn get_extension(&self, extension_type: ExtensionType) -> Option<&[u8]> {
+        self.extensions.get(&extension_type).map(|e| e.as_slice())
+    }
+
+    pub fn encode<W: Write + Unpin>(&self, buf: &mut W) -> std::io::Result<()> {
+        quiver_util::vli::write_int(buf, self.address_validation_data.len() as u64)?;
+        buf.write_all(&self.address_validation_data)?;
+
+        for (k, v) in self.extensions.iter() {
+            quiver_util::vli::write_int(buf, u64::from(*k))?;
+            quiver_util::vli::write_int(buf, v.len())?;
+            buf.write_all(v)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn decode<R: Read + Unpin>(buf: &mut R) -> std::io::Result<Self> {
+        let address_validation_data_len: usize = quiver_util::vli::read_int(buf)?;
+        let mut address_validation_data = vec![0u8; address_validation_data_len];
+        buf.read_exact(&mut address_validation_data)?;
+
+        let mut extensions = std::collections::BTreeMap::new();
+
+        loop {
+            let ext_id = match quiver_util::vli::read_int::<u64, _>(buf) {
+                Ok(i) => i.into(),
+                Err(e) => {
+                    if e.kind() == std::io::ErrorKind::UnexpectedEof {
+                        break
+                    } else {
+                        return Err(e);
+                    }
+                }
+            };
+
+            let ext_len: usize = quiver_util::vli::read_int(buf)?;
+            let mut ext_data = vec![0u8; ext_len];
+            buf.read_exact(&mut ext_data)?;
+
+            if extensions.insert(ext_id, ext_data).is_some() {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData, "Duplicate extension"
+                ));
+            }
+        }
+
+        Ok(Self {
+            address_validation_data,
+            extensions
+        })
+    }
+}
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct BDPToken {
     pub saved_capacity: u64,
     pub saved_rtt: u128,
     pub requested_capacity: u64,
-    pub address_validation_data: Vec<u8>,
-    pub bdp_data: Vec<u8>
+    pub private_data: Vec<u8>
 }
 
 impl Default for BDPToken {
@@ -23,19 +97,22 @@ impl Default for BDPToken {
             saved_capacity: 0,
             saved_rtt: 0,
             requested_capacity: 0,
-            address_validation_data: Vec::new(),
-            bdp_data: Vec::new()
+            private_data: Vec::new()
         }
     }
 }
 
 impl BDPToken {
-    pub fn encode<W: Write + Unpin>(&self, buf: &mut W) -> std::io::Result<()> {
-        quiver_util::vli::write_int(buf, self.address_validation_data.len() as u64)?;
-        buf.write_all(&self.address_validation_data)?;
+    pub fn saved_rtt_duration(&self) -> std::time::Duration {
+        std::time::Duration::new(
+            (self.saved_rtt / MICROS_PER_SEC) as u64,
+            ((self.saved_rtt % MICROS_PER_SEC) as u32) * NANOS_PER_MICRO
+        )
+    }
 
-        quiver_util::vli::write_int(buf, self.bdp_data.len() as u64)?;
-        buf.write_all(&self.bdp_data)?;
+    pub fn encode<W: Write + Unpin>(&self, buf: &mut W) -> std::io::Result<()> {
+        quiver_util::vli::write_int(buf, self.private_data.len() as u64)?;
+        buf.write_all(&self.private_data)?;
 
         quiver_util::vli::write_int(buf, self.saved_capacity)?;
         quiver_util::vli::write_int(buf, self.saved_rtt)?;
@@ -43,47 +120,62 @@ impl BDPToken {
         Ok(())
     }
 
-    pub fn decode<R: std::io::Read + Unpin>(buf: &mut R) -> std::io::Result<Self> {
-        let address_validation_data_len: usize = quiver_util::vli::read_int(buf)?;
-        let mut address_validation_data = vec![0u8; address_validation_data_len];
-        buf.read_exact(&mut address_validation_data)?;
-
-        let bdp_data_len: usize = quiver_util::vli::read_int(buf)?;
-        let mut bdp_data = vec![0u8; bdp_data_len];
-        buf.read_exact(&mut bdp_data)?;
+    pub fn decode<R: Read + Unpin>(buf: &mut R) -> std::io::Result<Self> {
+        let private_data_len: usize = quiver_util::vli::read_int(buf)?;
+        let mut private_data = vec![0u8; private_data_len];
+        buf.read_exact(&mut private_data)?;
 
         let saved_capacity = quiver_util::vli::read_int(buf)?;
         let saved_rtt = quiver_util::vli::read_int(buf)?;
         let requested_capacity = quiver_util::vli::read_int(buf)?;
 
         Ok(Self {
-            address_validation_data,
-            bdp_data,
+            private_data,
             saved_capacity,
             saved_rtt,
             requested_capacity,
         })
     }
+
+    pub fn from_quiche_cr_event(
+        event: &quiche::CREvent,
+        ip: std::net::IpAddr,
+        lifetime: chrono::Duration,
+        key: &[u8; 32]
+    ) -> Self {
+        let mut out = Self {
+            saved_capacity: event.cwnd as u64,
+            requested_capacity: event.cwnd as u64,
+            saved_rtt: event.min_rtt.as_micros(),
+            private_data: Vec::new()
+        };
+
+        let expiry = chrono::Utc::now() + lifetime;
+
+        let private_data = PrivateBDPData {
+            ip,
+            expiry,
+        };
+        private_data.encrypt(&mut out, key);
+
+        out
+    }
+
+    pub fn private_data(
+        &mut self,
+        key: &[u8; 32]
+    ) -> Option<PrivateBDPData> {
+        PrivateBDPData::decrypt(self, key)
+    }
 }
 
 #[derive(Debug)]
-pub struct CRBDPData {
-    saved_capacity: u64,
-    saved_rtt: std::time::Duration,
+pub struct PrivateBDPData {
     ip: std::net::IpAddr,
     expiry: chrono::DateTime<chrono::Utc>,
-    signature: Vec<u8>
 }
 
-impl CRBDPData {
-    pub fn saved_capacity(&self) -> u64 {
-        self.saved_capacity
-    }
-
-    pub fn saved_rtt(&self) -> std::time::Duration {
-        self.saved_rtt
-    }
-
+impl PrivateBDPData {
     pub fn ip(&self) -> std::net::IpAddr {
         self.ip
     }
@@ -96,122 +188,82 @@ impl CRBDPData {
         self.expiry < chrono::Utc::now()
     }
 
-    pub fn from_quiche_cr_event(
-        event: &quiche::CREvent,
-        ip: std::net::IpAddr,
-        lifetime: chrono::Duration,
-        key: &[u8]
-    ) -> Self {
-        let now = chrono::Utc::now();
-        let expiry = now + lifetime;
-
-        let mut out = Self {
-            saved_capacity: event.cwnd as u64,
-            saved_rtt: event.min_rtt,
-            ip,
-            expiry,
-            signature: vec![],
-        };
-
-        out.signature = out.make_mac(key).into_bytes().to_vec();
-
-        out
+    fn associated_data(bdp_token: &BDPToken) -> Vec<u8> {
+        let mut associated_data = std::io::Cursor::new(Vec::new());
+        associated_data.write_all(&bdp_token.saved_capacity.to_be_bytes()).unwrap();
+        associated_data.write_all(&bdp_token.saved_rtt.to_be_bytes()).unwrap();
+        associated_data.into_inner()
     }
 
-    pub fn to_bytes(&self) -> Vec<u8> {
-        let mut out = std::io::Cursor::new(Vec::new());
+    fn encrypt(&self, bdp_token: &mut BDPToken, key: &[u8; 32]) {
+        let cipher = chacha20poly1305::ChaCha20Poly1305::new(key.into());
 
-        let expiry_timestamp = self.expiry.timestamp();
-
-        out.write_all(&self.saved_capacity.to_be_bytes()).unwrap();
-        out.write_all(&self.saved_rtt.as_micros().to_be_bytes()).unwrap();
+        let mut data = std::io::Cursor::new(&mut bdp_token.private_data);
 
         match self.ip {
             std::net::IpAddr::V4(ip) => {
-                out.write_u8(0x04).unwrap();
-                // Only write the /24 subnet
-                out.write_all(&ip.octets()[..3]).unwrap();
+                data.write_u8(0x04).unwrap();
+                data.write_all(&ip.octets()).unwrap();
             }
             std::net::IpAddr::V6(ip) => {
-                out.write_u8(0x06).unwrap();
-                // Only write the /48 subnet
-                out.write_all(&ip.octets()[..6]).unwrap();
+                data.write_u8(0x06).unwrap();
+                data.write_all(&ip.octets()).unwrap();
             }
         }
 
-        out.write_all(&expiry_timestamp.to_be_bytes()).unwrap();
-        out.write_all(&self.signature).unwrap();
+        data.write_all(&self.expiry.timestamp().to_be_bytes()).unwrap();
 
-        out.into_inner()
+        let nonce = chacha20poly1305::ChaCha20Poly1305::generate_nonce(
+            &mut chacha20poly1305::aead::OsRng
+        );
+        cipher.encrypt_in_place(
+            &nonce, &Self::associated_data(bdp_token), &mut bdp_token.private_data
+        ).unwrap();
+        bdp_token.private_data.extend_from_slice(nonce.as_slice());
     }
 
-    pub fn from_bytes(data: &[u8]) -> std::io::Result<Self> {
-        let mut data = std::io::Cursor::new(data);
+    fn decrypt(bdp_token: &mut BDPToken, key: &[u8; 32]) -> Option<Self> {
+        let cipher = chacha20poly1305::ChaCha20Poly1305::new(key.into());
 
-        let saved_capacity = ReadBytesExt::read_u64::<BigEndian>(&mut data)?;
-        let saved_rtt = ReadBytesExt::read_u128::<BigEndian>(&mut data)?;
+        let data_len = bdp_token.private_data.len();
+        if data_len < 12 {
+            return None;
+        }
+        let nonce = <[u8; 12]>::try_from(bdp_token.private_data.drain(data_len-12..data_len)
+            .collect::<Vec<_>>()).unwrap();
 
-        let ip_ver = data.read_u8()?;
+        if let Err(_) = cipher.decrypt_in_place(
+            &nonce.into(), &Self::associated_data(bdp_token), &mut bdp_token.private_data
+        ) {
+            return None;
+        }
+
+        let mut data = std::io::Cursor::new(&bdp_token.private_data);
+
+        let ip_ver = data.read_u8().ok()?;
         let ip = match ip_ver {
             0x04 => {
                 let mut subnet = [0u8; 4];
-                data.read_exact(&mut subnet[..3])?;
+                data.read_exact(&mut subnet).ok()?;
                 std::net::IpAddr::V4(std::net::Ipv4Addr::from(subnet))
             }
             0x06 => {
                 let mut subnet = [0u8; 16];
-                data.read_exact(&mut subnet[..6])?;
+                data.read_exact(&mut subnet).ok()?;
                 std::net::IpAddr::V6(std::net::Ipv6Addr::from(subnet))
             }
-            _ => return Err(std::io::ErrorKind::InvalidData.into())
+            _ => return None
         };
 
-        let expiry_timestamp = ReadBytesExt::read_i64::<BigEndian>(&mut data)?;
+        let mut expiry_timestamp_bytes = [0u8; 8];
+        data.read_exact(&mut expiry_timestamp_bytes).ok()?;
+        let expiry_timestamp = i64::from_be_bytes(expiry_timestamp_bytes);
 
-        let mut signature = vec![0u8; HmacSha256::output_size()];
-        std::io::Read::read_exact(&mut data, &mut signature)?;
+        let expiry = chrono::DateTime::from_timestamp(expiry_timestamp, 0)?;
 
-        let saved_rtt = std::time::Duration::new(
-            (saved_rtt / MICROS_PER_SEC) as u64, ((saved_rtt % MICROS_PER_SEC) as u32) * NANOS_PER_MICRO
-        );
-        let expiry = chrono::DateTime::from_timestamp(expiry_timestamp, 0)
-            .ok_or_else(|| std::io::ErrorKind::InvalidData)?;
-
-        Ok(Self {
-            saved_capacity,
-            saved_rtt,
+        Some(Self {
             ip,
             expiry,
-            signature
         })
-    }
-
-    pub fn verify_signature(&self, key: &[u8]) -> bool {
-        if self.signature.len() != HmacSha256::output_size() {
-            return false;
-        }
-
-        let signature_array = generic_array::GenericArray::<
-            u8, <HmacSha256 as OutputSizeUser>::OutputSize
-        >::from_slice(&self.signature);
-
-        // Convert to constant time arrays for comparison
-        let calculated_signature = self.make_mac(key);
-        let stored_signature = digest::CtOutput::from(signature_array);
-
-        calculated_signature == stored_signature
-    }
-
-    fn make_mac(&self, key: &[u8]) -> digest::CtOutput<HmacSha256> {
-        use hmac::Mac;
-        let mut mac = HmacSha256::new_from_slice(key).unwrap();
-
-        let expiry_timestamp = self.expiry.timestamp();
-
-        mac.update(&self.saved_capacity.to_be_bytes());
-        mac.update(&self.saved_rtt.as_micros().to_be_bytes());
-        mac.update(&expiry_timestamp.to_be_bytes());
-
-       mac.finalize()
     }
 }
